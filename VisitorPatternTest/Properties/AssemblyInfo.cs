5d9b22724a7ea8c95392e3839739238f7e755175
AnduenaSh <anduenashasivari@gmail.com>
2024-12-03 11:46:10 +0100

    This project implements a Library Management System using the Visitor Design Pattern in C#. The system manages three types of library items: Books, Magazines, and Research Papers. Each item has specific properties and behavior.
    The Visitor Pattern allows the addition of new operations (like calculating fines) without modifying the existing library item classes. The design ensures adherence to key SOLID principles for maintainable and extensible code, except ISP.

---
+++ VisitorPatternTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("VisitorPatternTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("RIT Kosovo")]
+[assembly: AssemblyProduct("VisitorPatternTest")]
+[assembly: AssemblyCopyright("Copyright © RIT Kosovo 2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("d252dad0-9c12-4051-95f3-9b6a56f18c60")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
